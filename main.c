2c6ca5b45851e24b10f9c8fdebb1c89abba552d9
saidyanak <said.ynk@gmail.com>
2025-05-22 20:30:56 +0300

    argümanlar alınıyor " \' tırnaklara göre ayırma işlemi pars falan filan

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: syanak <syanak@student.42kocaeli.com.tr >  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 12:12:06 by yuocak            #+#    #+#             */
-/*   Updated: 2025/05/19 12:39:48 by syanak           ###   ########.fr       */
+/*   Updated: 2025/05/22 20:25:23 by syanak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,13 @@ int	main(int argc, char **argv, char **envp)
 		input = readline("minishell$ ");
 		if (!input)
 			break ;
-		tokenize_input(input, my_base);
+		tokenize_input(input, &my_base);
+		while (my_base.token)
+		{
+			printf("TOKEN: [%s] | TYPE: %d\n", my_base.token->content,
+				my_base.token->type);
+			my_base.token = my_base.token->next;
+		}
 		// execute_command(args, &my_env);
 		free(input);
 		// execute_command(args, &my_env);
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: syanak <syanak@student.42kocaeli.com.tr >  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 12:12:03 by yuocak            #+#    #+#             */
-/*   Updated: 2025/05/19 12:34:35 by syanak           ###   ########.fr       */
+/*   Updated: 2025/05/22 20:22:49 by syanak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,26 +18,31 @@
 # include <stdlib.h>
 
 // 🟩 Environment Linked List Yapısı
+
+typedef enum e_token_type
+{
+	TOKEN_WORD,
+	TOKEN_PIPE,
+	TOKEN_REDIRECT_IN,  // <
+	TOKEN_REDIRECT_OUT, // >
+	TOKEN_APPEND,       // >>
+	TOKEN_HEREDOC,      // <<
+	TOKEN_QUOTED_WORD
+}					t_token_type;
+
+typedef struct s_token
+{
+	char			*content;
+	t_token_type	type;
+	struct s_token	*next;
+}					t_token;
+
 typedef struct s_env
 {
 	char			*key;
 	char			*value;
 	struct s_env	*next;
 }					t_env;
-
-typedef struct s_cmd
-{
-	char			*cmd;
-	char			*param;
-	int				build_in;
-	struct t_cmd	*next;
-}					t_cmd;
-
-typedef struct s_token
-{
-	struct s_token	*next;
-
-}					t_token;
 
 typedef struct s_base
 {
@@ -49,13 +54,14 @@ typedef struct s_base
 t_env				*init_env(char **envp);
 t_env				*create_env_node(char *env_str);
 void				add_new_node(t_env **head, t_env *new_node);
-void				tokenize_input(char *input, t_base my_base);
+void				tokenize_input(char *input, t_base *my_base);
 
 // 🔧 String Yardımcı Fonksiyonlar
 char				*ft_strchr(const char *s, int c);
 char				*ft_strdup(const char *s1);
 char				*ft_strndup(const char *s1, size_t n);
-
+char				*ft_strjoin(char const *s1, char const *s2);
+char				*ft_substr(char const *s, unsigned int start, size_t len);
 // execute
 void				execute_command(char **args, t_env *my_env);
 
